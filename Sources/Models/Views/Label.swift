3b005317f039186d79d498486b592978ac89e3ce
Md. Ibrahim Hassan <mdibrahimhassan@gmail.com>
2021-05-19 15:11:06 +0530

    Fixed mapping issue in Label

--- Sources/Models/Views/Label.swift
+++ Sources/Models/Views/Label.swift
@@ -95,7 +95,7 @@ public struct Label: IBDecodable, ViewProtocol, IBIdentifiable {
             opaque: container.attributeIfPresent(of: .opaque),
             rect: container.elementIfPresent(of: .rect),
             subviews: container.childrenIfPresent(of: .subviews),
-            string: container.attributeIfPresent(of: .string),
+            string: container.elementIfPresent(of: .string),
             mutableString: container.elementIfPresent(of: .mutableString),
             text: container.attributeIfPresent(of: .text),
             textAlignment: container.attributeIfPresent(of: .textAlignment),
